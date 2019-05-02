a3a3c4ce7e5fbb18253543c2455680814c24a829
N Rosser <c3330570@uon.edu.au>
2019-05-02 04:42:28 +0000

    you can't......

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 
 int main() 
 {
- printf("hello world\n");
+    int f;
+for(f=0; f<55; f++)
+{
+ printf("not an engineer\n"); 
+}
  /*you should've asked FOR HELP EARLIER*/
- 
+ printf("i don't belong here...");
  return 0;
 }
